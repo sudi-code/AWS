06dda3ab0b0176b38f795bc364ebadfde5729035
sudi-code <30807510+sudi-code@users.noreply.github.com>
2017-08-10 13:48:00 +0530

    Update puppetmaster.sh

--- puppetmaster.sh
+++ puppetmaster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # PUPPET INSTALLATION AND CONFIGRATION OF AMAZON LINUX TO DEPLOY INSTANCES FROM PUPPET MANIEFESTS
-sudo yum update -y
-sudo yum install puppet-server -y
+yum update -y
+yum install puppet-server -y
 # COLLECT HOST DETAILS
 internalname="$(echo $HOSTNAME | awk -F'.' '{print $1}')"
 fqdn="hostname -f"
@@ -9,12 +9,12 @@ fqdn="hostname -f"
 echo "dns_alt_names = $internalname,$fqdn" >> /etc/puppet/puppet.conf
 echo "certname = $internalname" >> /etc/puppet/puppet.conf
 # START PUPPET MASTER
-sudo systemctl start puppetmaster
-sudo systemctl status puppetmaster
+systemctl start puppetmaster
+systemctl status puppetmaster
 # ENSURE PUPPET MASTER IS RUNNING
-sudo puppet resource service puppetmaster ensure=running
+puppet resource service puppetmaster ensure=running
 # GENERATE CA CERTS
-sudo -u puppet master --no-daemonize --verbose
-sudo puppet cert list --all
+puppet master --no-daemonize --verbose
+puppet cert list --all
 # INSTALL PUPPET AWS MODULE
-sudo puppet module install puppetlabs-aws
+puppet module install puppetlabs-aws
