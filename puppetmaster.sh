a3a27ba7089be1b9f437ec24d458e72714911e5d
sudi-code <30807510+sudi-code@users.noreply.github.com>
2017-08-10 14:20:19 +0530

    Update puppetmaster.sh

--- puppetmaster.sh
+++ puppetmaster.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-# PUPPET INSTALLATION AND CONFIGRATION OF AMAZON LINUX TO DEPLOY INSTANCES FROM PUPPET MANIEFESTS
+# Adding puppetrepo and installing the package
+sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 sudo yum update -y
 sudo yum install puppet-server -y
-# COLLECT HOST DETAILS
-sudo -i
+# Collecting ec2-instance details#
 internalname="$(echo $HOSTNAME | awk -F'.' '{print $1}')"
-fqdn=`hostname -f`
-# ADDING HOST SPECIFIC DETAILS TO PUPPET CONFIG
-echo "dns_alt_names = $internalname,$fqdn" >> /etc/puppet/puppet.conf
+hostname=$HOSTNAME
+# Adding ec2-instance details to the default configration file
+echo "dns_alt_names = $internalname,$hostname" >> /etc/puppet/puppet.conf
 echo "certname = $internalname" >> /etc/puppet/puppet.conf
-logout
-# ENSURE PUPPET MASTER IS RUNNING
-sudo puppet resource service puppetmaster ensure=running
-# GENERATE CA CERTS
-sudo puppet master --no-daemonize --verbose
-sudo puppet cert list --all
-# INSTALL PUPPET AWS MODULE
-sudo puppet module install puppetlabs-aws
+# Start the service and check status
+sudo systemctl start puppetmaster
+sudo systemctl status puppetmaster
+# Ensure puppetmaster service
+puppet resource service puppetmaster ensure=running
+# Generate CA CERTS
+puppet cert list --all
+#Install puppet modules for creation of new nodes
+puppet module install puppetlabs-aws
